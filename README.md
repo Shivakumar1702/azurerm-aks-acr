5ab3263cfeb9103b34dc8fbd3116e30f53fe620f
shiva <allamshivakumar009@gmail.com>
2023-12-14 22:42:02 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# azurerm-aks-acr
+This module is to create managed Azure Kubernetes cluster along with Azure container Registry.
+
+Azurerm provider version >=3.84.0
+
+Required variables:
+rg_name
+rg_location
+acr_name
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "client_certificate" {
   value     = azurerm_kubernetes_cluster.akscluster.kube_config.0.client_certificate
-  sensitive = true
+  description = "This output gives clinet certificate"
 }
 
 output "kube_config" {
   value     = azurerm_kubernetes_cluster.akscluster.kube_config_raw
-  sensitive = true
+  description = "This output gives the information about kube config"
 }
